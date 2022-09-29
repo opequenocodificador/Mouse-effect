e8d6d101d69296349b2b4c660c23e179590b569f
Monciaux Sylvain <monciauxsylvain63@gmail.com>
2022-09-29 16:16:53 +0100

    creation 2 circle

--- index.js
+++ index.js
@@ -6,11 +6,16 @@
 
 // Donner un style de transparence aux 2 plus gros ronds (mix-blend-mode)
 
-const cursor = document.querySelector(".cursor");
+const circle1 = document.querySelector(".circle1");
+
+const circle2 = document.querySelector(".circle2");
 
 window.addEventListener("mousemove", (e) => {
-  cursor.setAttribute(
-    "style",
-    "top:" + (e.pageY - 20) + "px;left:" + (e.pageX - 20) + "px;"
-  );
+  circle1.style.left = e.pageX + "px";
+  circle1.style.top = e.pageY + "px";
+});
+
+window.addEventListener("mousemove", (e) => {
+  circle2.style.left = e.pageX + "px";
+  circle2.style.top = e.pageY + "px";
 });
